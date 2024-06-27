77acfd58a0681408769941fc7eeba19fab7abb42
Mujam Akhil <mujam.akhil@globallogic.com>
2024-06-27 10:59:45 +0530

    this is my first repository to add into my github account

---
+++ src/main/java/com/example/demo/Controller/EmployeeController.java
@@ -0,0 +1,19 @@
+package com.example.demo.Controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/person")
+public class EmployeeController {
+
+    @GetMapping("/Employees")
+    public ResponseEntity<String> getEmployee()
+    {
+        return new ResponseEntity<>("employee details", HttpStatus.OK);
+    }
+}
